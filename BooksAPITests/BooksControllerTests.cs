2aff0d7be5b1838957f1af841b0defb21c0c3c1e
Zaimutiz <zaimutis@gmail.com>
2024-10-10 17:20:44 +0300

    Add files via upload

---
+++ BooksAPITests/BooksControllerTests.cs
@@ -0,0 +1,54 @@
+﻿using BooksAPI.Controllers;
+using BooksAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Moq;
+using Xunit;
+using BooksAPI;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Logging;
+
+namespace BooksAPITests
+{
+    public class BooksControllerTests
+    {
+        private readonly BooksController _controller;
+
+        public BooksControllerTests()
+        {
+            _controller = new BooksController();
+        }
+
+        [Fact]
+        public void GetAllBooks_ReturnsOkResult_WithBooks()
+        {
+            // Act
+            var result = _controller.GetAllBooks();
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result.Result);
+            var books = Assert.IsAssignableFrom<IEnumerable<Book>>(okResult.Value);
+            Assert.Equal(3, books.Count()); // Check initial book count
+        }
+
+        [Fact]
+        public void GetBookById_ExistingId_ReturnsOkResult()
+        {
+            // Arrange
+            int bookId = 1;
+
+            // Act
+            var result = _controller.GetBookById(bookId);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result.Result);
+            var book = Assert.IsType<Book>(okResult.Value);
+            Assert.Equal(bookId, book.Id);
+        }
+    }
+}
+        
